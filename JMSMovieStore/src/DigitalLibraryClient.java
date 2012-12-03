cd8bb34b2e18f367833bf1847b8200571531bac2
pawankjajara <pawankjajara@gmail.com>
2012-12-02 18:09:33 -0800

    Comment

--- JMSMovieStore/src/DigitalLibraryClient.java
+++ JMSMovieStore/src/DigitalLibraryClient.java
@@ -10,6 +10,7 @@ public class DigitalLibraryClient {
 	private Topic counterTopic;
 	private MessageConsumer consumer;
 	private Topic replyTopic;
+	private String Yash;
 	
 	public static void main(String args[])
 	{
